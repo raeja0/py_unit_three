032a785f5f9370684c335aec6ee740edd084de6a
raeja0 <raeja@ssfs.org>
2022-10-06 11:55:52 -0400

    Added some things

--- addition.py
+++ addition.py
@@ -1,8 +1,17 @@
 # Write your addition function in the space below...
-def add_two():
-    print("The sum of", num1, "and", )
+number_1 = input("First Number >> ")
+num1 = int(number_1)
+
+number_2 = input("Second Number >> ")
+num2 = int(number_2)
+
+total = num1 + num2
+
+def add_two(num1, num2):
+    print("The sum of", num1, "and", num2, "is", total)
 # Do not change anything below these lines
 def main():
+    add_two(num1, num2)
     pass
 
 if __name__ == '__main__':
