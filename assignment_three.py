d2882acef12b952796d7b472fb261888f4c95157
raeja0 <raeja@ssfs.org>
2022-10-18 10:28:12 -0400

    Tried fixing. didn't work.

--- assignment_three.py
+++ assignment_three.py
@@ -3,16 +3,44 @@ def startup():
 
 def length():
     l = float(input("Input length >> "))
-    return length()
+    return l
 
 def width():
     w = float(input("Input width >> "))
-    return width()
+    return w
 
 def height():
     h = float(input("Input height >> "))
-    return height()
+    return h
 
-def side1():
+def side1(l, w):
     tb = float((l * w)*2)
-    return side1
+    return tb
+
+def side2(w, h):
+    fb = float((w * h)*2)
+    return fb
+
+def side3(l, h):
+    lr = float((l * h)*2)
+    return lr
+
+def calculations(tb, fb, lr):
+    sa = (tb+fb+lr)
+    return sa
+
+def output(sa):
+    print("Total surface area:", sa)
+
+def main(l, w, h, tb, fb, lr, sa):
+    startup()
+    length()
+    width()
+    height()
+    side1(l, w)
+    side2(l, h)
+    side3(w, h)
+    calculations(tb, fb, lr)
+    output(sa)
+if __name__ == '__main__':
+    main(l, w, h, tb, fb, lr, sa)
--- return_addtion.py
+++ return_addtion.py
@@ -5,22 +5,22 @@ def inputs():
     l = float(input("Input length >> "))
     w = float(input("Input width >> "))
     h = float(input("Input height >> "))
+    return l, w, h
 
-def calculations():
+def calculations(l, w, h):
     tb = float((l * w)*2)
     lr = float((h * l)*2)
     fb = float((h * w)*2)
     sa = float(tb + lr + fb)
-def end_result():
-    return calculations()
+    return tb, lr, fb, sa
+def end_result(sa):
     print("Total surface area:", sa)
 
-def main():
+def main(l, w, h, sa):
     startup()
     inputs()
-    calculations()
-    end_result()
-
+    calculations(l, w, h)
+    end_result(sa)
 if __name__ == '__main__':
-    main()
+    main(l, w, h, sa)
 
